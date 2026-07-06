cb6b19f4c509fc5f124bba31f72807d460ae7dff
Ikramullah-portfolio <ikramullahcsweb@gmail.com>
2026-07-06 19:29:21 +0500

    Fixed api backup url for vercel

--- api.js
+++ api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// Yeh aapke live backend URL ko automatic catch kar lega
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://vercel.app",
     withCredentials: true // Cookies, sessions aur JWT tokens ke liye zaroori hai
 });
 
